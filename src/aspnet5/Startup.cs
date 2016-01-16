5696c2dea6262249c1177eee22ad8e7bb37539d2
jfraxedas <jfraxedas@gmail.com>
2016-01-16 15:04:36 -0500

    getting mvc going

--- src/aspnet5/Startup.cs
+++ src/aspnet5/Startup.cs
@@ -15,6 +15,9 @@ namespace aspnet5
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddCaching();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,10 +25,15 @@ namespace aspnet5
         {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello wonderful, but sometimes cruel world!");
-            });
+            app.UseSession();
+
+            app.UseMvc(routes =>  routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" }
+                    ));
+
+            
         }
 
         // Entry point for the application.
